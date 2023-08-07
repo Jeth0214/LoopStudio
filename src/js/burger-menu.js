2c690d5586fc46f34777caf64a91f8bf22c7e034
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-08-07 22:13:14 +0300

    working on navbar

---
+++ src/js/burger-menu.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector("#navToggle");
+
+navToggle.addEventListener("click", () => {
+  console.log("click");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        alata: ["Alata"],
+        josefin: ["Josefin Sans"],
+      },
+    },
+    container: {
+      center: true,
+      screens: {
+        lg: "1110px",
+        xl: "1110px",
+      },
+    },
+    fontSize: {
+      base: "15px",
+    },
   },
   plugins: [],
 };
