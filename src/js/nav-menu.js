092bde10f4eeef5eb66cd5d0033a0a22fe4dfaf9
Jeth0214 <rolandjethrosuyom@yahoo.com>
2023-08-09 22:17:15 +0300

    fixed navbar

--- src/js/nav-menu.js
+++ src/js/nav-menu.js
@@ -5,3 +5,10 @@ navToggle.addEventListener("click", () => {
   console.log("click");
   nav.classList.toggle("nav--active");
 });
+
+/** auto hide toggle menu and overlay when the user resize the window */
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 500) {
+    nav.classList.remove("nav--active");
+  }
+});
